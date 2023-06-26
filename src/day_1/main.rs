5d61dc285e5fea27251a4ed8391f483a45392926
Ankur Dubey <iam@ankurdubey.com>
2023-06-26 12:50:41 +0530

    Advent of Code Day 1

---
+++ src/day_1/main.rs
@@ -0,0 +1,23 @@
+use std::{cmp, env, fs};
+
+const FILE_PATH: &str = "./src/day_1/input.txt";
+
+fn main() {
+    let contents = fs::read_to_string(FILE_PATH).expect("Should have been able to read file");
+    let mut max_sum = 0;
+    let mut current_group_sum = 0;
+
+    for line in contents.split('\n') {
+        match line {
+            "" => {
+                max_sum = cmp::max(max_sum, current_group_sum);
+                current_group_sum = 0;
+            }
+            line => {
+                let value: i32 = line.parse().expect("Should be able to parse line as integer");
+                current_group_sum += value;
+            }
+        }
+    }
+    println!("{}", cmp::max(max_sum, current_group_sum));
+}
