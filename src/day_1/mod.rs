4bab87c113f99ebb5e1936b6fff04804c5554696
Ankur Dubey <iam@ankurdubey.com>
2023-06-27 10:55:40 +0530

    Error Handling

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,9 +1,19 @@
-use std::{cmp, fs};
+use std::{cmp, fs, io};
 
-pub fn execute() {
+pub struct Day1Result {
+    max: i32,
+}
+
+impl Day1Result {
+    pub fn show(self) {
+        println!("Day 1: Part 1: {}", self.max);
+    }
+}
+
+pub fn execute() -> Result<Day1Result, io::Error> {
     const FILE_PATH: &str = "./src/day_1/input.txt";
 
-    let contents = fs::read_to_string(FILE_PATH).expect("Should have been able to read file");
+    let contents = fs::read_to_string(FILE_PATH)?;
     let mut max_sum = 0;
     let mut current_group_sum = 0;
 
@@ -21,5 +31,7 @@ pub fn execute() {
             }
         }
     }
-    println!("Day 1 result: {}", cmp::max(max_sum, current_group_sum));
+    Ok(Day1Result {
+        max: cmp::max(max_sum, current_group_sum),
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use rust_advent_of_code_2022::day_1;
 
 fn main() {
-    day_1::execute();
+    let day1_result = day_1::execute();
+    day1_result
+        .expect("Should have been able to calculate day 1 result")
+        .show();
 }
